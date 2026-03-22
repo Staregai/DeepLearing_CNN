bc3214a87753340a2679546e7c193d8ed6665270
Michalina Majewska <01171318@pw.edu.pl>
2026-03-22 12:13:15 +0100

    mps device support

--- src/utils/device.py
+++ src/utils/device.py
@@ -1,5 +1,14 @@
 import torch
 
 
+def _get_device() -> str:
+    if torch.cuda.is_available():
+        return "cuda"
+    elif torch.backends.mps.is_available():
+        return "mps"
+    else:
+        return "cpu"
+
+
 def get_device() -> torch.device:
-    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    return torch.device(_get_device())
